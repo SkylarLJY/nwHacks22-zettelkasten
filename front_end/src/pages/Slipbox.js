d7c8472ef39f83ba8f9d04dc0dd77e7590f1df15
James Reinhardt <liverat@student.ubc.ca>
2022-01-15 22:03:11 -0800

    Send new files

---
+++ front_end/src/pages/Slipbox.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+import { makeStyles } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+import Radio from '@material-ui/core/Radio'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormControl from '@material-ui/core/FormControl'
+import FormLabel from '@material-ui/core/FormLabel'
+import { useHistory } from 'react-router-dom'
+import noteService from '../services/notes'
+
+const useStyles = makeStyles({
+
+})
+
+export default function Slipbox() {
+    return (
+        <Container size="sm">
+            <Typography
+                variant="h6"
+                color="textSecondary"
+                component="h2"
+                gutterBottom
+            >
+                Slipbox
+            </Typography>
+
+
+
+        </Container>
+    )
+
+}
