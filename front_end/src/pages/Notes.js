ab13e555c807c25da273168ba96a518860478c63
James Reinhardt <liverat@student.ubc.ca>
2022-01-15 20:05:52 -0800

    Changed foldername of front end

---
+++ front_end/src/pages/Notes.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import Container from '@material-ui/core/Container'
+import Masonry from 'react-masonry-css'
+import NoteCard from '../components/NoteCard'
+import noteService from '../services/notes'
+
+export default function Notes() {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('')
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+
+
+
+
+  const handleDelete = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = notes.filter(note => note.id !== id)
+
+    noteService
+      .deleteVal(id)
+      .then(() => {
+        setNotes(changedNote)
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1
+  };
+
+  return (
+    <Container>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column">
+        {notes.map(note => (
+          <div key={note.id}>
+            <NoteCard note={note} handleDelete={handleDelete} />
+          </div>
+        ))}
+      </Masonry>
+    </Container>
+  )
+}
